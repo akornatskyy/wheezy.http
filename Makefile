32a47493a6a2dd447de649fd3c352ad37d2dafad
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-17 16:55:27 +0300

    Initial project structure.

---
+++ Makefile
@@ -0,0 +1,57 @@
+.SILENT: clean env doc release test
+.PHONY: clean env doc release test
+
+VERSION=2.6
+PYTHON=env/bin/python$(VERSION)
+EASY_INSTALL=env/bin/easy_install-$(VERSION)
+PYTEST=env/bin/py.test-$(VERSION)
+NOSE=env/bin/nosetests-$(VERSION)
+SPHINX=env/bin/sphinx-build
+
+all: clean test release
+
+debian:
+	apt-get -yq update
+	apt-get -yq dist-upgrade
+	# How to Compile Python from Source
+	# http://mindref.blogspot.com/2011/09/compile-python-from-source.html
+	apt-get -yq install libbz2-dev build-essential python \
+		python-dev python-setuptools python-virtualenv \
+		mercurial
+
+env:
+	PYTHON_EXE=/usr/local/bin/python$(VERSION); \
+	if [ ! -x $$PYTHON_EXE ]; then \
+		PYTHON_EXE=/usr/bin/python$(VERSION); \
+	fi;\
+	virtualenv --python=$$PYTHON_EXE \
+		--no-site-packages env
+	$(EASY_INSTALL) coverage docutils nose \
+		pytest pytest-pep8 pytest-cov wsgiref \
+		sphinx
+
+clean:
+	find src/ -type d -name __pycache__ -exec rm -rf {} \; \
+		2>/dev/null
+	find src/ -name '*.py[co]' -delete
+	rm -rf dist/ build/ MANIFEST src/*.egg-info
+
+release:
+	$(PYTHON) setup.py -q bdist_egg
+
+test:
+	$(PYTEST) -q -x --pep8 --doctest-modules \
+		src/wheezy/http
+
+doctest-cover:
+	$(NOSE) --stop --with-doctest --detailed-errors \
+		--with-coverage --cover-package=wheezy.http \
+		src/wheezy/http/*.py
+
+test-cover:
+	$(PYTEST) -q --cov wheezy.http \
+		--cov-report term-missing \
+		src/wheezy/http/tests
+
+doc:
+	$(SPHINX) -a -b html doc/ doc/_build/
---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README')).read()
+
+setup(
+    name = 'wheezy-http',
+    version = '0.1',
+    description = 'A lightweight http request-response library',
+    long_description = README,
+    url = 'https://bitbucket.org/akorn/wheezy-http',
+
+    author = 'Andriy Kornatskyy',
+    author_email = 'andriy.kornatskyy at live.com',
+
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    keywords = [
+        'http', 'request', 'response', 'web'
+    ],
+    packages = ['wheezy', 'wheezy.http'],
+    package_dir = {'': 'src'},
+
+    zip_safe = True,
+    install_requires = [
+    ],
+    extras_require = {
+        'dev': [
+            'wsgiref',
+            'coverage',
+            'nose',
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov'
+        ]
+    },
+
+    platforms = 'any'
+)
