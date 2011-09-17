ba60eb82f4f22a4cbd5a7003cb37915731d15c8f
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-17 16:58:03 +0300

    defined compatibility functions with Python 3: BytesIO, STRING_EMPTY and ustr.

---
+++ src/wheezy/http/p2to3.py
@@ -0,0 +1,29 @@
+
+""" ``p2to3`` module.
+"""
+
+import sys
+
+
+PY3 = sys.version_info[0] == 3
+
+if PY3:  # pragma: nocover
+    from io import BytesIO
+
+    STRING_EMPTY = ''
+
+    def ustr(s, encoding):
+        if isinstance(s, bytes):
+            return str(s, encoding=encoding)
+        else:
+            return s
+else:  # pragma: nocover
+    from cStringIO import StringIO as BytesIO
+
+    STRING_EMPTY = unicode('')
+
+    def ustr(s, encoding):
+        if isinstance(s, unicode):
+            return s
+        else:
+            return unicode(s, encoding=encoding)
