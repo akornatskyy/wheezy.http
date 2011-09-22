057ae343c314dd7791e123a0590e235252858070
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-22 09:49:48 +0300

    Renamed RequestHeaders to HttpRequestHeaders, added HttpResponseHeaders

--- src/wheezy/http/headers.py
+++ src/wheezy/http/headers.py
@@ -2,8 +2,10 @@
 """ ``headers`` module.
 """
 
+from wheezy.http.p2to3 import bstr
 
-class RequestHeaders(object):
+
+class HttpRequestHeaders(object):
     """ Returns a header name from ``environ``
         for the keys started with HTTP_.
 
@@ -12,14 +14,14 @@ class RequestHeaders(object):
         http://tools.ietf.org/html/rfc4229
 
         >>> environ = {'HTTP_ACCEPT': 'text/plain'}
-        >>> h = RequestHeaders(environ)
+        >>> h = HttpRequestHeaders(environ)
         >>> h.ACCEPT
         'text/plain'
 
         >>> from wheezy.http import sample
         >>> environ = {}
         >>> sample.request_headers(environ)
-        >>> h = RequestHeaders(environ)
+        >>> h = HttpRequestHeaders(environ)
         >>> h.HOST
         'localhost:8080'
     """
@@ -33,3 +35,28 @@ class RequestHeaders(object):
         val = self.environ.get('HTTP_' + name, None)
         setattr(self, name, val)
         return val
+
+
+class HttpResponseHeaders(dict):
+    """ Any headers added are encoded to bytes.
+        http://www.python.org/dev/peps/pep-3333/#unicode-issues
+        http://www.faqs.org/rfcs/rfc2616.html
+    """
+
+    def __getitem__(self, header):
+        return super(HttpResponseHeaders, self).__getitem__(
+            bstr(header, 'iso-8859-1')
+        )
+
+    def __setitem__(self, header, value):
+        """
+            >>> from wheezy.http.p2to3 import ustr
+            >>> h = HttpResponseHeaders()
+            >>> h['Cache-Control'] = ustr('public', 'utf-8')
+            >>> v = h['Cache-Control']
+            >>> assert bstr('public', 'utf-8') == v
+        """
+        super(HttpResponseHeaders, self).__setitem__(
+            bstr(header, 'iso-8859-1'),
+            bstr(value, 'iso-8859-1')
+        )
