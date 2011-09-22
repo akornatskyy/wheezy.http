50ea252b1536288337bc427abdfc6eca963bbc2f
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-22 09:46:32 +0300

    Introduced cache policy module

---
+++ src/wheezy/http/cachepolicy.py
@@ -0,0 +1,503 @@
+
+""" ``cachepolicy`` module.
+"""
+
+from wheezy.http.date import format_http_datetime
+from wheezy.http.date import total_seconds
+from wheezy.http.utils import attribute
+
+
+SUPPORTED = {
+    'no-cache': 'no-cache',
+    'server': 'no-cache',
+    'private': 'private',
+    'both': 'private',  # server and private
+    'public': 'public'
+}
+
+
+class HttpCachePolicy(object):
+    """ Controls cache specific http headers.
+
+        >>> from datetime import datetime
+        >>> from wheezy.http.date import UTC
+        >>> when = datetime(2011, 9, 20, 15, 00, tzinfo=UTC)
+
+        ``Expires`` HTTP header:
+        >>> p = HttpCachePolicy()
+        >>> p.expires(when)
+        >>> p.HTTP_EXPIRES
+        'Tue, 20 Sep 2011 15:00:00 GMT'
+        >>> p = HttpCachePolicy('no-cache')
+        >>> p.HTTP_EXPIRES
+        '-1'
+
+        ``Last-Modified`` HTTP header:
+        >>> p = HttpCachePolicy()
+        >>> p.last_modified(when)
+        >>> p.HTTP_LAST_MODIFIED
+        'Tue, 20 Sep 2011 15:00:00 GMT'
+
+        ``Pragma`` HTTP header:
+
+        >>> p = HttpCachePolicy('no-cache')
+        >>> p.HTTP_PRAGMA
+        'no-cache'
+        >>> p = HttpCachePolicy()
+        >>> p.HTTP_PRAGMA
+
+        ``ETag`` Http header:
+
+        >>> p = HttpCachePolicy('public')
+        >>> p.etag('ABC')
+        >>> p.HTTP_ETAG
+        'ABC'
+
+        ``Vary`` Http header:
+        >>> p = HttpCachePolicy('public')
+        >>> p.vary()
+        >>> p.HTTP_VARY
+        '*'
+
+        ``Cache-Control`` HTTP header:
+
+        >>> p = HttpCachePolicy('public')
+        >>> p.HTTP_CACHE_CONTROL
+        'public'
+        >>> p = HttpCachePolicy('public')
+        >>> p.private('a', 'b')
+        >>> p.HTTP_CACHE_CONTROL
+        'public, private="a, b"'
+        >>> p = HttpCachePolicy('public')
+        >>> p.no_cache('c', 'd')
+        >>> p.HTTP_CACHE_CONTROL
+        'public, no-cache="c, d"'
+        >>> p = HttpCachePolicy('no-cache')
+        >>> p.no_store()
+        >>> p.no_transform()
+        >>> p.HTTP_CACHE_CONTROL
+        'no-cache, no-store, no-transform'
+        >>> p = HttpCachePolicy('no-cache')
+        >>> p.must_revalidate()
+        >>> p.HTTP_CACHE_CONTROL
+        'no-cache, must-revalidate'
+        >>> p = HttpCachePolicy('no-cache')
+        >>> p.proxy_revalidate()
+        >>> p.HTTP_CACHE_CONTROL
+        'no-cache, proxy-revalidate'
+        >>> p = HttpCachePolicy()
+        >>> p.append_extension('ext1')
+        >>> p.append_extension('ext2')
+        >>> p.HTTP_CACHE_CONTROL
+        'private, ext1, ext2'
+        >>> p = HttpCachePolicy()
+        >>> p.max_age(60)
+        >>> p.HTTP_CACHE_CONTROL
+        'private, max-age=60'
+        >>> p = HttpCachePolicy()
+        >>> p.smax_age(15)
+        >>> p.HTTP_CACHE_CONTROL
+        'private, smax-age=15'
+    """
+
+    def __init__(self, cacheability='private'):
+        """ Initialize cache policy with a given cacheability.
+
+            If cacheability is not supported raise ValueError.
+
+            >>> HttpCachePolicy('x') # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+        """
+        if cacheability not in SUPPORTED.keys():
+            raise ValueError('Invalid cacheability.')
+        self.cacheability = cacheability
+        self.is_no_cache = cacheability in ('server', 'no-cache')
+        self.is_public = cacheability == 'public'
+
+        self.HTTP_PRAGMA = self.is_no_cache and 'no-cache' or None
+        self.HTTP_EXPIRES = self.is_no_cache and '-1' or None
+        self.HTTP_LAST_MODIFIED = None
+        self.HTTP_ETAG = None
+        self.vary_headers = []
+        self.private_fields = []
+        self.no_cache_fields = []
+        self.is_no_store = False
+        self.is_must_revalidate = False
+        self.is_proxy_revalidate = False
+        self.is_no_transform = False
+        self.extensions = []
+        self.max_age_delta = -1
+        self.smax_age_delta = -1
+
+    def update(self, headers):
+        """ Updates ``headers`` with this cache policy.
+
+            No cache headers:
+
+            >>> headers = {}
+            >>> p = HttpCachePolicy('no-cache')
+            >>> p.update(headers)
+            >>> headers # doctest: +NORMALIZE_WHITESPACE
+            {'Expires': '-1', 'Pragma': 'no-cache',
+                    'Cache-Control': 'no-cache'}
+
+            Public caching headers:
+
+            >>> from datetime import datetime, timedelta
+            >>> from wheezy.http.date import UTC
+            >>> when = datetime(2011, 9, 20, 15, 00, tzinfo=UTC)
+            >>> headers = {}
+            >>> p = HttpCachePolicy('public')
+            >>> p.last_modified(when)
+            >>> p.expires(when + timedelta(hours=1))
+            >>> p.etag('abc')
+            >>> p.vary()
+            >>> p.update(headers)
+            >>> headers # doctest: +NORMALIZE_WHITESPACE
+            {'Last-Modified': 'Tue, 20 Sep 2011 15:00:00 GMT',
+            'ETag': 'abc', 'Expires': 'Tue, 20 Sep 2011 16:00:00 GMT',
+            'Vary': '*', 'Cache-Control': 'public'}
+
+            If haders doesn't support ``__setitem__`` protocol
+            raise TypeError.
+
+            >>> p = HttpCachePolicy()
+            >>> p.update('') # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            TypeError: ...
+        """
+        if not hasattr(headers, '__setitem__'):
+            raise TypeError('headers must implement '
+                    '__setitem__ protocol')
+        headers['Cache-Control'] = self.HTTP_CACHE_CONTROL
+        if self.HTTP_PRAGMA:
+            headers['Pragma'] = self.HTTP_PRAGMA
+        if self.HTTP_EXPIRES:
+            headers['Expires'] = self.HTTP_EXPIRES
+        if self.HTTP_LAST_MODIFIED:
+            headers['Last-Modified'] = self.HTTP_LAST_MODIFIED
+        if self.HTTP_ETAG:
+            headers['ETag'] = self.HTTP_ETAG
+        if self.HTTP_VARY:
+            headers['Vary'] = self.HTTP_VARY
+
+    def fail_no_cache(self, option):
+        if self.is_no_cache:
+            raise ValueError(option + ' is not valid '
+                'for server or no-cache cacheability')
+
+    def assert_public(self, option):
+        if not self.is_public:
+            raise ValueError(option + ' is valid for '
+                'public cacheability only')
+
+    def private(self, *fields):
+        """ Indicates that part of the response message is
+            intended for a single user and MUST NOT be
+            cached by a shared cache.
+
+            Only valid for ``public`` cacheability.
+
+            >>> p = HttpCachePolicy('public')
+            >>> p.private('a', 'b')
+            >>> p.private_fields
+            ['a', 'b']
+
+            Otherwise raise error.
+
+            >>> p = HttpCachePolicy('no-cache')
+            >>> p.private('a') # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+        """
+        if fields:
+            self.assert_public('private field(s)')
+            self.private_fields += fields
+
+    def no_cache(self, *fields):
+        """ The specified field-name(s) MUST NOT be sent in the
+            response to a subsequent request without succsessful
+            revalidation with the origin server.
+
+            >>> p = HttpCachePolicy('public')
+            >>> p.no_cache('a', 'b')
+            >>> p.no_cache_fields
+            ['a', 'b']
+
+            Not valid for ``server`` or ``no-cache``
+            cacheability.
+
+            >>> p = HttpCachePolicy('no-cache')
+            >>> p.no_cache('a') # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+          """
+        if fields:
+            self.fail_no_cache('no-cache fields')
+            self.no_cache_fields += fields
+
+    def no_store(self):
+        """ The purpose of the no-store directive is to
+            prevent the inadvertent release or retention of
+            sensitive information.
+
+            >>> p = HttpCachePolicy()
+            >>> p.no_store()
+            >>> assert p.is_no_store
+        """
+        self.is_no_store = True
+
+    def must_revalidate(self):
+        """ Because a cache MAY be configured to ignore a
+            server's specified expiration time, and because a
+            client request MAY include a max-stale directive
+            (which has a similar effect), the protocol also
+            includes a mechanism for the origin server to
+            require revalidation of a cache entry on any
+            subsequent use.
+
+            >>> p = HttpCachePolicy()
+            >>> p.must_revalidate()
+            >>> assert p.is_must_revalidate
+
+            Raises ValueError if proxy-revalidave is set.
+
+            >>> p = HttpCachePolicy()
+            >>> p.is_proxy_revalidate = True
+            >>> p.must_revalidate() # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+              ...
+            ValueError: ...
+        """
+        if self.is_proxy_revalidate:
+            raise ValueError('proxy-revalidate is already set')
+        self.is_must_revalidate = True
+
+    def proxy_revalidate(self):
+        """ The proxy-revalidate directive has the same
+            meaning as the must- revalidate directive,
+            except that it does not apply to non-shared
+            user agent caches.
+
+            >>> p = HttpCachePolicy()
+            >>> p.proxy_revalidate()
+            >>> assert p.is_proxy_revalidate
+
+            Raises ValueError if must-revalidave is set.
+
+            >>> p = HttpCachePolicy()
+            >>> p.is_must_revalidate = True
+            >>> p.proxy_revalidate() # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+              ...
+            ValueError: ...
+        """
+        if self.is_must_revalidate:
+            raise ValueError('must-revalidate is already set')
+        self.is_proxy_revalidate = True
+
+    def no_transform(self):
+        """ The cache or proxy MUST NOT change any aspect
+            of the entity-body that is specified by this header,
+            including the value of the entity-body itself.
+
+            >>> p = HttpCachePolicy()
+            >>> p.no_transform()
+            >>> assert p.is_no_transform
+        """
+        self.is_no_transform = True
+
+    def append_extension(self, extension):
+        """ Appends the ``ext`` to the Cache-Control HTTP header.
+
+            >>> p = HttpCachePolicy()
+            >>> p.append_extension('ext')
+            >>> assert 'ext' in p.extensions
+        """
+        self.extensions.append(extension)
+
+    def max_age(self, delta):
+        """ Accept a response whose age is no greater than the
+            specified time in seconds.
+
+            ``delta`` can be ``int`` or ``datetime.timedelta``.
+
+            >>> p = HttpCachePolicy()
+            >>> p.max_age(100)
+            >>> p.max_age_delta
+            100
+
+            Not valid for ``server`` for ``no-cache`` cacheability,
+            raise ValueError.
+
+            >>> p = HttpCachePolicy('no-cache')
+            >>> p.max_age(100) # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+        """
+        self.fail_no_cache('max-age')
+        self.max_age_delta = total_seconds(delta)
+
+    def smax_age(self, delta):
+        """ If a response includes an s-maxage directive, then
+            for a shared cache (but not for a private cache),
+            the maximum age specified by this directive overrides
+            the maximum age specified by either the max-age
+            directive or the Expires header. Accept a response whose
+            age is no greater than the specified time in seconds.
+
+            ``delta`` can be ``int`` or ``datetime.timedelta``.
+
+            >>> p = HttpCachePolicy()
+            >>> p.smax_age(100)
+            >>> p.smax_age_delta
+            100
+
+            Not valid for ``server`` or ``no-cache`` cacheability,
+            raise ValueError.
+
+            >>> p = HttpCachePolicy('no-cache')
+            >>> p.smax_age(100) # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+        """
+        self.fail_no_cache('smax-age')
+        self.smax_age_delta = total_seconds(delta)
+
+    def expires(self, when):
+        """ The Expires entity-header field gives the date/time
+            after which the response is considered stale.
+
+            >>> from datetime import datetime
+            >>> from wheezy.http.date import UTC
+            >>> p = HttpCachePolicy()
+            >>> when = datetime(2011, 9, 20, 13, 30, tzinfo=UTC)
+            >>> p.expires(when)
+            >>> p.HTTP_EXPIRES
+            'Tue, 20 Sep 2011 13:30:00 GMT'
+
+            Not valid for ``server`` or ``no-cache`` cacheability,
+            raise ValueError.
+
+            >>> p = HttpCachePolicy('no-cache')
+            >>> p.expires(when) # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+        """
+        self.fail_no_cache('expires')
+        self.HTTP_EXPIRES = format_http_datetime(when)
+
+    def last_modified(self, when):
+        """ The Last-Modified entity-header field indicates the
+            date and time at which the origin server believes
+            the variant was last modified.
+
+            >>> from datetime import datetime
+            >>> from wheezy.http.date import UTC
+            >>> p = HttpCachePolicy()
+            >>> when = datetime(2011, 9, 20, 15, 1, tzinfo=UTC)
+            >>> p.last_modified(when)
+            >>> p.HTTP_LAST_MODIFIED
+            'Tue, 20 Sep 2011 15:01:00 GMT'
+
+            Not valid for ``server`` or ``no-cache`` cacheability,
+            raise ValueError.
+
+            >>> p = HttpCachePolicy('no-cache')
+            >>> p.last_modified(when) # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+        """
+        self.fail_no_cache('expires')
+        self.HTTP_LAST_MODIFIED = format_http_datetime(when)
+
+    def etag(self, tag):
+        """ Provides the current value of the entity tag for the
+            requested variant.
+
+            >>> p = HttpCachePolicy('public')
+            >>> p.etag('ABC')
+            >>> p.HTTP_ETAG
+            'ABC'
+
+            Valid only for ``public`` cacheability, raise ValueError.
+
+            >>> p = HttpCachePolicy()
+            >>> p.etag('ABC') # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+        """
+        self.assert_public('etag')
+        self.HTTP_ETAG = tag
+
+    def vary(self, *headers):
+        """ The Vary field value indicates the set of request-header
+            fields that fully determines, while the response is fresh,
+            whether a cache is permitted to use the response to reply
+            to a subsequent request without revalidation.
+
+            >>> p = HttpCachePolicy('public')
+            >>> p.vary('Accept-Encoding', 'Accept-Language')
+            >>> p.HTTP_VARY
+            'Accept-Encoding, Accept-Language'
+
+            Vary by star (*):
+            >>> p = HttpCachePolicy('public')
+            >>> p.vary()
+            >>> p.HTTP_VARY
+            '*'
+
+            Valid only for ``public`` cacheability, raise ValueError.
+
+            >>> p = HttpCachePolicy()
+            >>> p.vary() # doctest: +ELLIPSIS
+            Traceback (most recent call last):
+                ...
+            ValueError: ...
+        """
+        self.assert_public('vary')
+        if headers:
+            self.vary_headers += headers
+        else:
+            self.HTTP_VARY = '*'
+
+    @attribute
+    def HTTP_VARY(self):
+        return ', '.join(self.vary_headers)
+
+    @attribute
+    def HTTP_CACHE_CONTROL(self):
+        """ Returns a value for Cache-Control headers.
+        """
+        directives = [SUPPORTED[self.cacheability]]
+        if self.private_fields:
+            directives.append('private="%s"'
+                    % ', '.join(self.private_fields))
+        if self.no_cache_fields:
+            directives.append('no-cache="%s"'
+                    % ', '.join(self.no_cache_fields))
+        if self.is_no_store:
+            directives.append('no-store')
+        if self.is_must_revalidate:
+            directives.append('must-revalidate')
+        elif self.is_proxy_revalidate:
+            directives.append('proxy-revalidate')
+        if self.is_no_transform:
+            directives.append('no-transform')
+        if self.extensions:
+            directives.append(', '.join(self.extensions))
+        if self.max_age_delta >= 0:
+            directives.append('max-age=' + str(self.max_age_delta))
+        if self.smax_age_delta >= 0:
+            directives.append('smax-age=' + str(self.smax_age_delta))
+        return ', '.join(directives)
