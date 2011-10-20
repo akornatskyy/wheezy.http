8fb1147b69101b890c56862c975d900bde463ba5
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-10-20 20:58:35 +0200

    Removed unused compatibility function due to move of crypto to security package.

--- src/wheezy/http/comp.py
+++ src/wheezy/http/comp.py
@@ -15,20 +15,10 @@ if PY3:  # pragma: nocover
         """
         return n.encode(encoding)
 
-    def ntou(n, encoding):
-        """ Converts native string to unicode
-        """
-        return n
-
     def bton(b, encoding):
         """ Converts bytes to native string
         """
         return b.decode(encoding)
-
-    chr = lambda i: bytes([i])
-    ord = lambda b: b
-    b = lambda s: s.encode('latin1')
-    n = lambda s: s.decode('latin1')
 
 else:  # pragma: nocover
     from cStringIO import StringIO as BytesIO
@@ -38,20 +28,10 @@ else:  # pragma: nocover
         """
         return n
 
-    def ntou(n, encoding):
-        """ Converts native string to unicode
-        """
-        return n.decode(encoding)
-
     def bton(b, encoding):
         """ Converts bytes to native string
         """
         return b
-
-    chr = chr
-    ord = ord
-    b = lambda s: s
-    n = lambda s: s
 
 
 if PY3:  # pragma: nocover
@@ -83,48 +63,3 @@ else:  # pragma: nocover
 
     def parse_qs(qs, encoding):
         return _parse_qs(qs, keep_blank_values=True)
-
-
-# Hash functions
-try:  # pragma: nocover
-    # Python 2.5+
-    from hashlib import md5
-    from hashlib import sha1
-    digest_size = lambda d: d().digest_size
-except ImportError:  # pragma: nocover
-    import md5
-    import sha as sha1
-    digest_size = lambda d: d.digest_size
-
-
-# Encryption interface
-block_size = None
-encrypt = None
-decrypt = None
-
-# Supported cyphers
-aes128 = None
-aes192 = None
-aes256 = None
-
-# Python Cryptography Toolkit (pycrypto)
-try:  # pragma: nocover
-    from Crypto.Cipher import AES
-
-    # pycrypto interface
-    block_size = lambda c: c.block_size
-    encrypt = lambda c, v: c.encrypt(v)
-    decrypt = lambda c, v: c.decrypt(v)
-
-    # suppored cyphers
-    def aes(key, key_size=32):
-        key = key[-key_size:]
-        iv = key[-16:]
-        return lambda: AES.new(key, AES.MODE_CBC, iv)
-
-    aes128 = lambda key: aes(key, 16)
-    aes192 = lambda key: aes(key, 24)
-    aes256 = lambda key: aes(key, 32)
-except ImportError:  # pragma: nocover
-    # TODO: add fallback to other encryption providers
-    pass
