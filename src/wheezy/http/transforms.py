5673aaa8ac1e998e7247b49c1529a8f66e2bec00
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-01-16 19:47:19 +0200

    Customized gzip_transform with compress_level, min_length and vary

--- src/wheezy/http/transforms.py
+++ src/wheezy/http/transforms.py
@@ -5,23 +5,40 @@
 from wheezy.core.collections import gzip_iterator
 
 
-def gzip_transform(request, response):
-    if response.skip_body:
-        return response
-    chunks = response.buffer
-    if len(chunks) == 0 or len(chunks[0]) < 1024:
-        return response
-    if 'gzip' in request.environ.get('HTTP_ACCEPT_ENCODING', ''):
-        # text or script
-        if response.content_type[0] == 't' \
-                or response.content_type[-2:] == 'pt':
+def gzip_transform(compress_level=6, min_length=1024, vary=False):
+    """ Allows gzip compression.
+
+        ``compress_level`` - the compression level, between 1 and 9, where 1
+        is the least compression (fastest) and 9 is the most (slowest)
+
+        ``min_length`` - sets the minimum length, in bytes, of the
+        first chunk in response that will be compressed. Responses
+        shorter than this byte-length will not be compressed.
+
+        ``vary`` - enables response header "Vary: Accept-Encoding".
+    """
+    def gzip(request, response):
+        if response.skip_body:
+            return response
+        chunks = response.buffer
+        if len(chunks) == 0 or len(chunks[0]) < min_length:
+            return response
+        # HTTP/1.1
+        if request.environ['SERVER_PROTOCOL'][-1] == '1' and (
+                response.content_type[0] == 't'
+                or response.content_type[-2:] == 'pt'
+                ) and 'gzip' in request.environ.get(
+                        'HTTP_ACCEPT_ENCODING', ''):
+            # text or script
             response.headers.append(('Content-Encoding', 'gzip'))
-            cache_policy = response.cache
-            if cache_policy:
-                if cache_policy.is_public:
-                    cache_policy.vary('Accept-Encoding')
-            response.buffer = tuple(gzip_iterator(chunks))
-    return response
+            response.buffer = tuple(gzip_iterator(chunks, compress_level))
+            if vary:
+                cache_policy = response.cache
+                if cache_policy:
+                    if cache_policy.is_public:
+                        cache_policy.vary('Accept-Encoding')
+        return response
+    return gzip
 
 
 def response_transforms(transform=None, transforms=None):
