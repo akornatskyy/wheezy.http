2fce072565bf4d3b4516d8bab077b0339c4b8822
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-03-04 10:48:06 +0200

    Refactored httpcache by moved it to HTTPCacheMiddleware.

--- src/wheezy/http/cache.py
+++ src/wheezy/http/cache.py
@@ -2,199 +2,33 @@
 """ ``cache`` module
 """
 
-from wheezy.core.datetime import parse_http_datetime
 from wheezy.http.response import HTTP_HEADER_CONTENT_LENGTH_ZERO
 
 
-def response_cache(profile, cache=None):
+def response_cache(profile):
     """ Decorator that applies cache profile strategy to the
         wrapping handler.
     """
-    def decorate(func):
-        handler = httpcache(
-                factory=func,
-                cache_profile=profile,
-                cache=cache)
+    def decorate(handler):
+        if not profile.enabled:
+            return handler
 
-        def call(request, *args, **kwargs):
-            if args or kwargs:
-                response = func(request, *args, **kwargs)
-            else:
-                response = handler(request)
-            return response
-        return call
-    return decorate
-
-
-def httpcache(factory, cache_profile, cache=None):
-    """ cache factory that selects appropriate cache strategy
-        according to cache profile settings.
-
-        Disabled
-
-        >>> from wheezy.http.cacheprofile import CacheProfile
-        >>> cacheprofile = CacheProfile('none', enabled=False)
-        >>> httpcache('factory', cacheprofile, 'cache')
-        'factory'
-
-        No cache strategy
-
-        >>> from wheezy.http.response import HTTPResponse
-        >>> def factory(request):
-        ...     return HTTPResponse()
-        >>> cacheprofile = CacheProfile('none')
-        >>> result = httpcache(factory, cacheprofile, 'cache')
-        >>> result.__name__
-        'nocache_strategy'
-
-        Get or set strategy
-
-        >>> from wheezy.core.collections import attrdict
-        >>> class Cache(object):
-        ...     def __init__(self, response=None):
-        ...         self.response = response
-        ...     def get(self, key):
-        ...         return self.response
-        ...     def set_multi(self, mapping, time):
-        ...         pass
-        >>> request = attrdict(method='GET', path='/abc')
-        >>> cache = Cache(response='x')
-        >>> cacheprofile = CacheProfile('server', duration=100)
-        >>> result = httpcache(factory, cacheprofile, cache)
-        >>> result.__name__
-        'get_or_set_strategy'
-    """
-    if cache_profile.enabled:
-        if cache_profile.request_vary:
-            def get_or_set_strategy(*args, **kwargs):
-                kwargs['cache'] = cache
-                kwargs['cache_profile'] = cache_profile
-                kwargs['factory'] = factory
-                return get_or_set2(*args, **kwargs)
-            return get_or_set_strategy
+        if profile.request_vary:
+            def cache(request, *args, **kwargs):
+                response = handler(request, *args, **kwargs)
+                response.cache_profile = profile
+                if response.cache_policy is None:
+                    response.cache_policy = profile.cache_policy()
+                return response
+            return cache
         else:
-            def nocache_strategy(*args, **kwargs):
-                kwargs['cache_profile'] = cache_profile
-                kwargs['factory'] = factory
-                return nocache(*args, **kwargs)
-            return nocache_strategy
-    else:
-        return factory
-
-
-def nocache(request, cache_profile, factory):
-    """ No cache strategy.
-
-        CachePolicy is set if response status code is 200.
-
-        >>> from wheezy.http.cacheprofile import CacheProfile
-        >>> from wheezy.http.response import HTTPResponse
-        >>> def factory(request):
-        ...     return HTTPResponse()
-        >>> cp = CacheProfile('none')
-        >>> response = nocache(None, cp, factory)
-        >>> assert response.cache
-    """
-    response = factory(request)
-    if response.status_code == 200:
-        if response.cache is None:
-            response.cache = cache_profile.cache_policy()
-    return response
-
-
-def get_or_set(request, cache, cache_profile, factory):
-    """ Get or set cache strategy.
-
-        Cache hit.
-
-        >>> from wheezy.core.collections import attrdict
-        >>> from wheezy.http.cacheprofile import CacheProfile
-        >>> from wheezy.http.response import HTTPResponse
-        >>> class Cache(object):
-        ...     def __init__(self, response=None):
-        ...         self.response = response
-        ...     def get(self, key):
-        ...         return self.response
-        ...     def set_multi(self, mapping, time):
-        ...         pass
-        >>> request = attrdict(method='GET', path='/abc')
-        >>> cache_profile = CacheProfile('server', duration=100)
-        >>> cache = Cache(response='x')
-        >>> factory = None
-        >>> get_or_set(request, cache, cache_profile, factory)
-        'x'
-
-        Cache miss.
-
-        >>> cache = Cache(response=None)
-        >>> factory = lambda r: HTTPResponse()
-        >>> response = get_or_set(request, cache, cache_profile, factory)
-        >>> assert isinstance(response, CacheableResponse)
-
-        Cache dependency.
-
-        >>> class CacheDependency(object):
-        ...     def next_key(self): return 'k'
-        >>> response = HTTPResponse()
-        >>> response.dependency = CacheDependency()
-        >>> factory = lambda r: response
-        >>> response = get_or_set(request, cache, cache_profile, factory)
-        >>> assert isinstance(response, CacheableResponse)
-    """
-    request_vary = cache_profile.request_vary
-    request_key = request_vary.key(request)
-    response = cache.get(request_key)
-    if response:  # cache hit
-        return response
-    response = factory(request)
-    if response.status_code == 200:
-        if response.cache is None:
-            response.cache = cache_profile.cache_policy()
-        mapping = {}
-        dependency = response.dependency
-        if dependency:
-            mapping[dependency.next_key()] = request_key
-        response = CacheableResponse(response)
-        mapping[request_key] = response
-        cache.set_multi(mapping, cache_profile.duration)
-    return response
-
-
-def get_or_set2(request, cache, cache_profile, factory):
-    """ cache strategy that supports IF_MODIFIED_SINCE and
-        IF_NONE_MATCH HTTP request headers.
-    """
-    request_vary = cache_profile.request_vary
-    request_key = request_vary.key(request)
-    response = cache.get(request_key)
-    if response:  # cache hit
-        if response.last_modified:
-            environ = request.environ
-            modified_since = environ.get('HTTP_IF_MODIFIED_SINCE', None)
-            if modified_since:
-                modified_since = parse_http_datetime(modified_since)
-                if modified_since >= response.last_modified:
-                    return NotModifiedResponse(response)
-        if response.etag:
-            none_match = environ.get('HTTP_IF_NONE_MATCH', None)
-            if none_match and response.etag in none_match:
-                return NotModifiedResponse(response)
-        return response
-    response = factory(request)
-    if response.status_code == 200:
-        if response.cache is None:
-            response.cache = cache_profile.cache_policy()
-        mapping = {}
-        dependency = response.dependency
-        if dependency:
-            mapping[dependency.next_key()] = request_key
-        middleware_vary = cache_profile.middleware_vary
-        if middleware_vary:
-            mapping['C' + middleware_vary.key(request)] = cache_profile
-        response = CacheableResponse(response)
-        mapping[request_key] = response
-        cache.set_multi(mapping, cache_profile.duration)
-    return response
+            def no_cache(request, *args, **kwargs):
+                response = handler(request, *args, **kwargs)
+                if response.cache_policy is None:
+                    response.cache_policy = profile.cache_policy()
+                return response
+            return no_cache
+    return decorate
 
 
 class NotModifiedResponse(object):
@@ -272,8 +106,8 @@ class CacheableResponse(object):
         def capture_headers(status, headers):
             self.headers = headers
         self.buffer = tuple(response(capture_headers))
-        cache_policy = response.cache
-        if cache_policy:
+        cache_policy = response.cache_policy
+        if cache_policy is not None:
             self.last_modified = cache_policy.modified
             self.etag = cache_policy.http_etag
 
--- src/wheezy/http/middleware.py
+++ src/wheezy/http/middleware.py
@@ -2,7 +2,10 @@
 """ ``middleware`` module.
 """
 
-from wheezy.http.cache import httpcache
+from wheezy.core.collections import defaultdict
+from wheezy.core.datetime import parse_http_datetime
+from wheezy.http.cache import CacheableResponse
+from wheezy.http.cache import NotModifiedResponse
 from wheezy.http.cacheprofile import RequestVary
 
 
@@ -13,23 +16,56 @@ class HTTPCacheMiddleware(object):
     def __init__(self, cache, middleware_vary):
         """
             ``cache`` - cache to be used.
-            ``middleware_vary`` - a way to determine cache key
-            for the request.
+            ``middleware_vary`` - a way to determine cache profile
+            key for the request.
         """
         assert cache
         assert middleware_vary
         self.cache = cache
         self.middleware_vary = middleware_vary
+        self.profiles = defaultdict()
 
     def __call__(self, request, following):
-        assert following is not None
-        cache_profile = self.cache.get(
-                'C' + self.middleware_vary.key(request))
-        if cache_profile is None:
-            return following(request)
-        else:
-            handler = httpcache(following, cache_profile, self.cache)
-            return handler(request)
+        middleware_key = 'C' + self.middleware_vary.key(request)
+        cache_profile = self.profiles.get(middleware_key, None)
+        if cache_profile:
+            request_key = cache_profile.request_vary.key(request)
+            response = self.cache.get(request_key)
+            if response:  # cache hit
+                if response.last_modified:
+                    environ = request.environ
+                    modified_since = environ.get(
+                            'HTTP_IF_MODIFIED_SINCE', None)
+                    if modified_since:
+                        modified_since = parse_http_datetime(modified_since)
+                        if modified_since >= response.last_modified:
+                            return NotModifiedResponse(response)
+                if response.etag:
+                    none_match = environ.get('HTTP_IF_NONE_MATCH', None)
+                    if none_match and response.etag in none_match:
+                        return NotModifiedResponse(response)
+                return response
+        response = following(request)
+        if response.status_code == 200:
+            response_cache_profile = response.cache_profile
+            if response_cache_profile:
+                if cache_profile != response_cache_profile:
+                    self.profiles[middleware_key] = response_cache_profile
+                    request_key = response_cache_profile.request_vary.key(
+                            request)
+                dependency = response.dependency
+                response = CacheableResponse(response)
+                if dependency:
+                    self.cache.set_multi({
+                        request_key: response,
+                        dependency.next_key(): request_key
+                        }, response_cache_profile.duration)
+                else:
+                    self.cache.set(
+                        request_key,
+                        response,
+                        response_cache_profile.duration)
+        return response
 
 
 def http_cache_middleware_factory(options):
