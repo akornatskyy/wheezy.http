81d2bdbeee7ba1782676bcb8c7bf6819fb061b6c
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-04-17 09:53:40 +0300

    Moved HTTPRequestHeaders from headers to request module.

--- src/wheezy/http/request.py
+++ src/wheezy/http/request.py
@@ -7,9 +7,48 @@ from wheezy.core.url import UrlParts
 
 from wheezy.http.comp import bton
 from wheezy.http.comp import parse_qs
-from wheezy.http.headers import HTTPRequestHeaders
 from wheezy.http.parse import parse_cookie
 from wheezy.http.parse import parse_multipart
+
+
+class HTTPRequestHeaders(object):
+    """ Returns a header name from ``environ``
+        for the variables started with ``HTTP_``.
+
+        Variables corresponding to the client-supplied
+        HTTP request headers (i.e., variables whose names
+        begin with ``HTTP_``). The presence or absence of these
+        variables corresponds with the presence or
+        absence of the appropriate HTTP header in the request.
+
+        Attributes correspond to appropriate HTTP headers
+        in the request.
+
+        See complete list of HTTP header fields in
+        `rfc4229 <http://tools.ietf.org/html/rfc4229>`_.
+
+        >>> environ = {'HTTP_ACCEPT': 'text/plain'}
+        >>> h = HTTPRequestHeaders(environ)
+        >>> h.ACCEPT
+        'text/plain'
+        >>> h['ACCEPT']
+        'text/plain'
+        >>> h['X']
+    """
+
+    def __init__(self, environ):
+        self.environ = environ
+
+    def __getitem__(self, name):
+        try:
+            return self.environ['HTTP_' + name]
+        except KeyError:
+            return None
+
+    def __getattr__(self, name):
+        val = self[name]
+        setattr(self, name, val)
+        return val
 
 
 class HTTPRequest(object):
