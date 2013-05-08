4d8085944489143d584df63ac1e4b1738af9359b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2013-05-08 12:35:24 +0300

    Minor refactoring in demo wsgi_adapter for etag_func.

--- demos/wsgi_adapter/wsgi_adapter.py
+++ demos/wsgi_adapter/wsgi_adapter.py
@@ -6,11 +6,15 @@
     $ env/bin/python wsgi_adapter.py
 """
 
+from datetime import datetime
+from datetime import timedelta
+
 from wheezy.caching import MemoryCache
 from wheezy.http import CacheProfile
-from wheezy.http import HTTPCachePolicy
 from wheezy.http import WSGIApplication
 from wheezy.http import bootstrap_http_defaults
+from wheezy.http.cache import etag_md5crc32
+from wheezy.http.cache import wsgi_cache
 from wheezy.http.comp import b
 from wheezy.http.middleware import environ_cache_adapter_middleware_factory
 from wheezy.http.middleware import http_cache_middleware_factory
@@ -18,22 +22,22 @@ from wheezy.http.middleware import wsgi_adapter_middleware_factory
 
 
 cache = MemoryCache()
-cache_factory = lambda: cache
-cache_profile = CacheProfile('both', duration=60)
+cache_profile = CacheProfile('both',
+                             enabled=True,
+                             etag_func=etag_md5crc32,
+                             duration=timedelta(seconds=10))
 
 
+@wsgi_cache(profile=cache_profile)
 def welcome(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    policy = HTTPCachePolicy('public')
-    policy.etag('12345')
-    environ['wheezy.http.cache_policy'] = policy
-    environ['wheezy.http.cache_profile'] = cache_profile
-    return [b('Hello World!!!')]
+    return [b('Hello World!!!\nThe time is %s' %
+              datetime.now().isoformat(' '))]
 
 
 options = {
     'wsgi_app': welcome,
-    'http_cache_factory': cache_factory
+    'http_cache': cache
 }
 main = WSGIApplication([
     bootstrap_http_defaults,
@@ -44,9 +48,11 @@ main = WSGIApplication([
 
 
 if __name__ == '__main__':
+    from wsgiref.handlers import BaseHandler
     from wsgiref.simple_server import make_server
     try:
         print('Visit http://localhost:8080/')
+        BaseHandler.http_version = '1.1'
         make_server('', 8080, main).serve_forever()
     except KeyboardInterrupt:
         pass
