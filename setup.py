9cb81d56424e8c53cfcbd93445845db58649e7e8
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2014-01-19 17:36:31 +0200

    Cythonize python modules in setup.

--- setup.py
+++ setup.py
@@ -7,6 +7,17 @@ try:
 except:
     from distutils.core import setup  # noqa
 
+
+extra = {}
+try:
+    from Cython.Build import cythonize
+    path = os.path.join('src', 'wheezy', 'http')
+    extra['ext_modules'] = cythonize(
+        [os.path.join(path, '*.py')],
+        quiet=True)
+except ImportError:
+    pass
+
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
@@ -69,5 +80,6 @@ setup(
         ]
     },
 
-    platforms='any'
+    platforms='any',
+    **extra
 )
